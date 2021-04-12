b6c16765aced5467989450191bf679711e9defea
DESKTOP-RDD88MA\LG <tjsdk2486@gmil.com>
2021-04-13 02:01:17 +0900

    예제4. 구구단5단

---
+++ 예제4/예제4/Source.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main() {
+	int x = 5;
+
+	printf("5*1=%d\n", x * 1);
+	printf("5*2=%d\n", x * 2);
+	printf("5*3=%d\n", x * 3);
+	printf("5*4=%d\n", x * 4);
+	printf("5*5=%d\n", x * 5);
+	printf("5*6=%d\n", x * 6);
+	printf("5*7=%d\n", x * 7);
+	printf("5*8=%d\n", x * 8);
+	printf("5*9=%d\n", x * 9);
+	
+	fgetc(stdin);
+	
+
+	
+
+}
